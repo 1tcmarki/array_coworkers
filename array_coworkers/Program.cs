e79e3dd7dee6e682fe2efb76818999eae59e0a1d
zwwxxxxx <126754383+1tcmarki@users.noreply.github.com>
2024-03-25 11:19:10 +0100

    Add project files.

---
+++ array_coworkers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_coworkers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Zoltan : Tomb letrehozasa
+            //Mate : Tomb adatainak osszeadasa
+            //Balint : Eredmenyek kiirasa
+
+
+        }
+    }
+}
